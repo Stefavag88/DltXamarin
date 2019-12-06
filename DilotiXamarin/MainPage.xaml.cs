5f32b34205299b83da48c902dcfb9049e14a1ba5
Vangelis Stefanakis <stefanakisvag@gmail.com>
2019-12-07 01:06:48 +0200

    first working commit

---
+++ DilotiXamarin/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using ReactiveUI.XamForms;
+using System.ComponentModel;
+
+
+namespace DilotiXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ReactiveContentPage<MainPageViewModel>
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainPageViewModel();
+        }
+
+        
+    }
+}
---
+++ DilotiXamarin/MainPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using DynamicData;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+
+namespace DilotiXamarin
+{
+    public class MainPageViewModel: ReactiveObject
+    {
+        private int _currentNum;
+        public int CurrentNum
+        {
+            get => _currentNum;
+            set => this.RaiseAndSetIfChanged(ref _currentNum, value);
+        }
+
+        private Stack<int> _history = new Stack<int>();
+        public Stack<int> History
+        {
+            get => _history;
+            set => this.RaisePropertyChanged(nameof(HistoryString));
+        }
+
+        public string HistoryString => string.Join("     ", History);
+
+        public ReactiveCommand<string, int> UpdateCommand { get; }
+
+        public MainPageViewModel()
+        {
+            History = new Stack<int>();
+
+            UpdateCommand = ReactiveCommand.Create<string, int>((p) => 
+            {   
+                switch (p)
+                {
+                    case "+":
+                        History.Push(_currentNum);
+                        this.RaisePropertyChanged(nameof(HistoryString));
+                        return CurrentNum++;
+                    case "-":
+                        History.Push(_currentNum);
+                        this.RaisePropertyChanged(nameof(HistoryString));
+                        return CurrentNum--;
+                    case "C":
+                        History.Clear();
+                        this.RaisePropertyChanged(nameof(HistoryString));
+                        CurrentNum = 0;
+                        return CurrentNum;
+                    case "U":
+                        CurrentNum = History.Count > 0 ? History.Pop() : 0;
+                        this.RaisePropertyChanged(nameof(HistoryString));
+                        return CurrentNum;
+                    default:
+                        return CurrentNum;
+                }
+            });
+        }
+
+    }
+}
