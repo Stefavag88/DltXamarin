d4968760532994d1336da10a5ea1e26bfe5389d6
Vangelis Stefanakis <stefanakisvag@gmail.com>
2019-12-07 12:37:04 +0200

    Show cards in listview

---
+++ DilotiXamarin/HandView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI.XamForms;
+using Xamarin.Forms.Xaml;
+
+namespace DilotiXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HandView : ReactiveContentView<HandViewModel>
+    {
+        public HandView()
+        {
+            InitializeComponent();
+
+            ViewModel = new HandViewModel();
+        }
+    }
+}
---
+++ DilotiXamarin/HandViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DynamicData;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace DilotiXamarin
+{
+    public class HandViewModel : ReactiveObject
+    {
+        private SourceList<Card> _myCards = new SourceList<Card>();
+        //public IObservableList<Card> MyCards => _myCards.AsObservableList();
+
+        public ObservableCollection<Card> MyCards => new ObservableCollection<Card>
+        {
+             new Card { ImagePath = "Assets/cards_svg/10C.svg", Name="10C" },
+             new Card { ImagePath = "Assets/cards_svg/2D.svg", Name="2D" },
+             new Card { ImagePath = "Assets/cards_svg/5H.svg", Name="5H" }
+        };
+
+        public HandViewModel()
+        {
+            _myCards.AddRange(new List<Card>
+            {
+                new Card { ImagePath = "Assets/cards_svg/10C.svg" },
+                new Card { ImagePath = "Assets/cards_svg/2D.svg" },
+                new Card { ImagePath = "Assets/cards_svg/5H.svg" }
+            });
+        }
+
+    }
+
+    public class Card
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
